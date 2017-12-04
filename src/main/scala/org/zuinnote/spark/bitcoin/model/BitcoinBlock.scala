fc2f2552130d4d87578438248dfa50fe7e337059
Jörn Franke <jornfranke@gmail.com>
2017-12-05 00:06:45 +0100

    Added header license information

--- src/main/scala/org/zuinnote/spark/bitcoin/model/BitcoinBlock.scala
+++ src/main/scala/org/zuinnote/spark/bitcoin/model/BitcoinBlock.scala
@@ -1,3 +1,23 @@
+/**
+  * Copyright 2017 ZuInnoTe (Jörn Franke) <zuinnote@gmail.com>
+  *
+  * Licensed under the Apache License, Version 2.0 (the "License");
+  * you may not use this file except in compliance with the License.
+  * You may obtain a copy of the License at
+  *
+  * http://www.apache.org/licenses/LICENSE-2.0
+  *
+  * Unless required by applicable law or agreed to in writing, software
+  * distributed under the License is distributed on an "AS IS" BASIS,
+  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  * See the License for the specific language governing permissions and
+  * limitations under the License.
+**/
+
+/**
+* Author: Omer van Kloeten (https://github.com/omervk)
+**/
+
 package org.zuinnote.spark.bitcoin.model
 
 final case class Input(prevTransactionHash: Array[Byte], previousTxOutIndex: Long, txInScriptLength: Array[Byte],
@@ -12,7 +32,7 @@ final case class ScriptWitnessItem(stackItemCounter: Array[Byte], scriptWitnessL
 final case class Transaction(version: Int, marker: Byte, flag: Byte, inCounter: Array[Byte], outCounter: Array[Byte],
                              listOfInputs: Seq[Input], listOfOutputs: Seq[Output],
                              listOfScriptWitnessItem: Seq[ScriptWitnessItem], lockTime: Int) {
-  
+
   private[bitcoin] def enriched(currentTransactionHash: Array[Byte]): EnrichedTransaction = {
     EnrichedTransaction(
       version, marker, flag, inCounter, outCounter, listOfInputs, listOfOutputs, listOfScriptWitnessItem, lockTime,
